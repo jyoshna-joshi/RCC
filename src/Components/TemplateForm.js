45b33f9590c9e85dc68b0fe8e6b483853aba7d49
jyoshna-joshi <g.jyoshnajoshi@gmail.com>
2023-04-26 18:48:37 +1000

    Template type form designed

--- src/Components/TemplateForm.js
+++ src/Components/TemplateForm.js
@@ -1,27 +1,79 @@
 import { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const items = ["Pdf", "cd", "Others"];
 
 const TemplateForm = () => {
-        const [selectedItem, setSelectedItem] = useState("Select Template Type");
+    const [selectedItem, setSelectedItem] = useState("Select Template Type");
     return (
         <>
-          
-            <Dropdown>
-                <h3>What type of content you want to upload?</h3>
-                <Dropdown.Toggle variant="success"> {selectedItem}</Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {items.map((item) => (
-                        <Dropdown.Item onClick={() => setSelectedItem(item)}>
-                            {item}
-                        </Dropdown.Item>
-                    ))}
-                </Dropdown.Menu>
-            </Dropdown>
-            <pre>You choose: {selectedItem}</pre>
-            
+            <h3>Content application form</h3>
+            <Form>
+                {/* for template type */}
+                <Form.Group className="mb-3" controlId="formTemplateType">
+                    <Form.Label>What type of content you want to upload?</Form.Label>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="secondary"> {selectedItem}</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {items.map((item) => (
+                                <Dropdown.Item onClick={() => setSelectedItem(item)}>
+                                    {item}
+                                </Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                        <pre>You choose: {selectedItem}</pre>
+                    </Dropdown>
+                </Form.Group>
+
+
+                {/* for title */}
+                <Form.Group className="mb-3" controlId="formTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" />
+                </Form.Group>
+
+                {/* for author*/}
+                <Form.Group className="mb-3" controlId="formAuthorName">
+                    <Form.Label>Author Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter author name" />
+                </Form.Group>
+
+                {/* for short description */}
+                <Form.Group className="mb-3" controlId="formShortDescription">
+                    <Form.Label>Short description</Form.Label>
+                    <Form.Control type="text" placeholder="Enter short description" />
+                </Form.Group>
+
+                {/* for year*/}
+                <Form.Group className="mb-3" controlId="formYear">
+                    <Form.Label>Year</Form.Label>
+                    <Form.Control type="text" placeholder="Enter year" />
+                </Form.Group>
+
+                {/* for uploading image*/}
+                <Form.Group className="mb-3" controlId="formUploadImage">
+                    <Button variant="secondary" type="button">
+                        Upload Image
+                    </Button>
+                </Form.Group>
+
+                {/* for uploading file*/}
+                <Form.Group className="mb-3" controlId="formUploadFile">
+                    <Button variant="secondary" type="button">
+                        Upload file
+                    </Button>
+                </Form.Group>
+
+                {/* for submit for approval*/}
+                <Form.Group className="mb-3" controlId="formSubmitForApproval">
+                    <Button variant="primary" type="submit">
+                        Submit for approval
+                    </Button>
+                </Form.Group>
+            </Form >
         </>
     );
 };
