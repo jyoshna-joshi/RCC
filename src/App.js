f57c1edf0236ad915de5a00eeac0b798fb672a51
nediri <n.edirisinghe@e-analytics.com>
2023-04-28 16:43:57 +1000

    Admin Dashboard initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import Message from './Components/Message';
-import Welcome from './Components/Welcome';
+import React, { Component, Suspense } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './scss/style.scss'
+
+const AdminLayout = React.lazy(() => import('./layout/AdminLayout'))
+// const Home = React.lazy(() => import('./layout/AdminLayout'))
 
 function App() {
   return (
     <div className="App">
-      <Welcome/>
-      <Message/>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route exact path="/" name="Home" element={<Home />} /> */}
+          <Route exact path="*" name="Admin Dashboard" element={<AdminLayout />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
