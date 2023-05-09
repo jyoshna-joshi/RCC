47467cd1be1258f45ada058da54ff8f8c948ae41
UChethy <chethys@gmail.com>
2023-05-10 00:24:48 +1000

    Add route path

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,16 @@ import React from 'react'
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const AllUploads = React.lazy(() => import('./views/content/AllUploads'))
 const PendingApprovals = React.lazy(() => import('./views/content/PendingApprovals'))
-const UploadContent = React.lazy(() => import('./components/UploadTemplateForm'))
+const SaveTempleteContent = React.lazy(() => import('./components/SaveTempleteContent'))
 const ApproveContent = React.lazy(() => import('./components/AdminApproval'))
 
 const routes = [
   { path: '/', name: 'Dashboard1', element: Dashboard },
   { path: '/admin/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/content/all', name: 'All Uploads', element: AllUploads },
-  { path: '/content/pending', name: 'Pending Approvals', element: PendingApprovals },
-  { path: '/content/upload', name: 'Upload Content', element: UploadContent },
+  { path: '/content/pending', name: 'Pending Approvals', element: PendingApprovals },  
+  { path: '/content/upload', name: 'Upload Content', element: SaveTempleteContent },
+ // { path: '/content/upload', name: 'Upload Content', element: UploadContent },
   { path: '/content/approve', name: 'Approve Content', element: ApproveContent },
 ]
 
