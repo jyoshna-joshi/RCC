1d128337eb23f4f43aeb0dd58faaef3ce5c916de
UChethy <chethys@gmail.com>
2023-05-12 15:30:58 +1000

    api integration for ApprovalPDF

--- src/Components/ApprovalPDF.js
+++ src/Components/ApprovalPDF.js
@@ -8,14 +8,74 @@ import Tab from 'react-bootstrap/Tab';
 import Col from 'react-bootstrap/Col';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { ListGroup, ListGroupItem, Spinner } from 'react-bootstrap';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
+import { clear } from '@testing-library/user-event/dist/clear';
 
 function ApprovalPDF() {
     const [pdfFile, setPDFFile] = useState(null)
     const [viewPdf, setViewPdf] = useState(null)
     const fileType = ['application/pdf']
+    const navigate = useNavigate();
+    const URL_GetTemplateTypes = "http://44.202.58.84:3000/template/types";
+    const URL_FIELD_TYPE = "http://44.202.58.84:3000/template/fields?type=";
+    const URL_UpdateContentStatus = "http://44.202.58.84:3000/content/update-status/:id";
+    const URL_ListByStatus = "http://44.202.58.84:3000/content/list-by-status?status=Pending";
+    //for dynamic fields
+    const [fields, setFields] = useState([]);
+    const [content, setContent] = useState([]);
+    //for dynamic templatetypes
+    const [types, setTypes] = useState([]);
+    //for selected template type
+    const [selectedTemplateType, setSelectedTemplateType] = useState("Select Template Type");
+
+    /**
+        * api integration for template type
+        */
+    useEffect(() => {
+        axios.get(URL_GetTemplateTypes)
+            .then(response => {
+                setTypes(response.data);
+                fetchUserData(response.data[0]);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+
+    /**
+     * fetch forms data from api
+     * @param {type of template} templateType 
+     */
+    function fetchUserData(templateType) {
+        setFields([]);
+
+        fetch(URL_FIELD_TYPE + templateType)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setFields(data.fields)
+            })
+        setSelectedTemplateType(templateType);
+    }
+
+    /**
+     * param {event} event 
+     * param {position} index 
+     */
+
+    const handleInputChange = (event, index) => {
+        fields[index]["value"] = event.target.value;
+        setFields(fields);
+    };
+
     const handleChange = (e) => {
         let selectedFile = e.target.files[0]
         if (selectedFile) {
@@ -56,17 +116,26 @@ function ApprovalPDF() {
                 {/* for title*/}
                 <Form.Group className="mb-3" controlId="advertisementJournalTitle">
                     <Form.Label>Title</Form.Label>
-                    <Form.Control required type="text" placeholder="Title name" />
+                    <Form.Label >  </Form.Label>
+                    {
+                        //<Form.Control required type="text" placeholder="Title name" />
+                    }
                 </Form.Group>
                 {/* for subject*/}
                 <Form.Group className="mb-3" controlId="advertisementJournalSubject">
                     <Form.Label>Subject</Form.Label>
-                    <Form.Control required type="text" placeholder="Subject" />
+                    <Form.Label >  </Form.Label>
+                    {
+                        //<Form.Control required type="text" placeholder="Subject" />
+                    }
                 </Form.Group>
                 {/* for publisher*/}
                 <Form.Group className="mb-3" controlId="advertisementJournalPublisher">
                     <Form.Label>Publisher</Form.Label>
-                    <Form.Control required type="text" placeholder="Journal name" />
+                    <Form.Label >  </Form.Label>
+                    {
+                        //<Form.Control required type="text" placeholder="Journal name" />
+                    }
                 </Form.Group>
                 <Form.Label>Pending Article</Form.Label>
                 <Form.Control required type="file" onChange={handleChange} />
