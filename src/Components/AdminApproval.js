a43bd42c21d43c6ea127f63f7f5dbd162bdac55b
UChethy <chethys@gmail.com>
2023-05-14 00:43:11 +1000

    Approval PDF Approve and Reject

--- src/Components/AdminApproval.js
+++ src/Components/AdminApproval.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ListGroup, ListGroupItem, Spinner } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
@@ -13,69 +12,65 @@ import ApprovalVideo from './ApprovalVideo';
 import Card from 'react-bootstrap/Card';
 
 
-function AdminApproval() {
-    //for spinner
-    //const [isLoading, setIsLoading] = useState(false);
-                                    
 
-    class AdminApproval extends Component {
-        constructor(props) {
-            super(props);
-        }
+class AdminApproval extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-        render() {
-            const submitTemplate = () => {
-                return (
-                    alert("API Call")
-                );
-            };
-
-
+    render() {
+        const submitTemplate = () => {
             return (
-              <Card>
-                    <Tab.Container id="list-group-tabs" defaultActiveKey="#approvalImage"  >
-                        <h4 className='AdminApproval-form' style={{ color: 'blueviolet' }}>Approve or reject uploaded content</h4>
-                        <Row>
-                            <Col sm={3} />
-                            <Col sm={2} className='Template-text'>
-                                <ListGroup>
-                                    <ListGroup.Item action href="#approvalPDF">
-                                        Approve Document articles
-                                    </ListGroup.Item>
-                                    <ListGroup.Item action href="#approvalImage">
-                                        Approve Image articles
-                                    </ListGroup.Item>
-                                    <ListGroup.Item action href="#approvalVideo">
-                                        Approve Video articles
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Col>
-                            <Col sm={6} className='Template-text'>
-                                <Tab.Content>
-                                {/*loadSpinner()}
-                                    {/* ApprovalPDF */}
-                                    <Tab.Pane eventKey="#approvalPDF">
-                                        <ApprovalPDF />
-                                    </Tab.Pane>
-                                    {/* ApprovalImage */}
-                                    <Tab.Pane eventKey="#approvalImage">
-                                        <ApprovalImage />
-                                    </Tab.Pane>
-                                    {/* ApprovalVideo */}
-                                    <Tab.Pane eventKey="#approvalVideo">
-                                        <ApprovalVideo />
-                                    </Tab.Pane>
-                                </Tab.Content>
-
-                            </Col>
-                            <Col sm={3} />
-
-                        </Row>
-                    </Tab.Container>
-                    </Card>
+                alert("API Call")
             );
-        }
+        };
+
+
+        return (
+            <Card>
+            <Tab.Container id="list-group-tabs" defaultActiveKey="#approvalPDF"  >
+                <h4 className='AdminApproval-form' style={{ color: 'blueviolet' }}>Approve or reject uploaded content</h4>
+                <Row>
+                    <Col sm={3} />
+                    <Col sm={2} className='Template-text'>
+                        <h6>Please choose type of article to approve</h6>
+                        <ListGroup>
+                            <ListGroup.Item action href="#approvalPDF">
+                                Approve Document articles
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="#approvalImage">
+                                Approve Image articles
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="#approvalVideo">
+                                Approve Video articles
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col sm={6} className='Template-text'>
+                        <Tab.Content>
+                            {/* ApprovalPDF */}
+                            <Tab.Pane eventKey="#approvalPDF">
+                                <ApprovalPDF />
+                            </Tab.Pane>
+                            {/* ApprovalImage */}
+                            <Tab.Pane eventKey="#approvalImage">
+                                <ApprovalImage />
+                            </Tab.Pane>
+                            {/* ApprovalVideo */}
+                            <Tab.Pane eventKey="#approvalVideo">
+                                <ApprovalVideo />
+                            </Tab.Pane>
+                        </Tab.Content>
+
+                    </Col>
+                    <Col sm={1} />
+
+                </Row>
+            </Tab.Container>
+            </Card>
+        );
     }
 }
+
 
 export default AdminApproval;
--- src/Components/ApprovalPDF.js
+++ src/Components/ApprovalPDF.js
@@ -8,14 +8,49 @@ import Tab from 'react-bootstrap/Tab';
 import Col from 'react-bootstrap/Col';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
+import { clear } from '@testing-library/user-event/dist/clear';
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { DataTable } from 'primereact/datatable';
+import { Calendar } from 'primereact/calendar';
+import { MultiSelect } from 'primereact/multiselect';
+import { Tag } from 'primereact/tag';
+import Card from 'react-bootstrap/Card';
+
 
 function ApprovalPDF() {
     const [pdfFile, setPDFFile] = useState(null)
     const [viewPdf, setViewPdf] = useState(null)
     const fileType = ['application/pdf']
+    const navigate = useNavigate();
+    const URL_UpdateContentStatus = "http://44.202.58.84:3000/content/update-status/6455fdcec77562dfb4e62808?status=Rejected";
+    const URL_ListByStatus = "http://44.202.58.84:3000/content/list-by-status?status=Pending";
+
+    //for dynamic fields
+    const [data, setData] = useState([]);
+    const [id, setID] = useState();
+    /**
+     * Fetch pending list by status
+     */
+    useEffect(() => {
+        const fetchData = async () => {
+            var url = 'http://44.202.58.84:3000/content/645e3d78f97604e432a915c9'
+
+            const idTemp = "645e3d78f97604e432a915c9";
+            setID(idTemp);
+            const res = await fetch(url);
+            const resdata = await res.json();
+            console.log(resdata.creator);
+            setData(resdata);
+        }
+        fetchData();
+    }, []
+    );
     const handleChange = (e) => {
         let selectedFile = e.target.files[0]
         if (selectedFile) {
@@ -36,7 +71,15 @@ function ApprovalPDF() {
         }
     }
 
-
+    const handleApprove = async (stat) => {
+        var url = 'http://44.202.58.84:3000/content/update-status/' + id;
+        const res = await fetch(url, {
+            method: 'POST',
+            body : JSON.stringify({status:stat})
+        })
+        const resdata = await res;
+        console.log(resdata);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -50,18 +93,19 @@ function ApprovalPDF() {
     const newplugin = defaultLayoutPlugin()
     return (
 
-        <Form onSubmit={handleSubmit}>
+        <Card >
             {/* for Choosing file for approval*/}
             <Form.Group className="Template-text" controlId="JournalViewforApproval" >
                 {/* for title*/}
                 <Form.Group className="mb-3" controlId="advertisementJournalTitle">
                     <Form.Label>Title</Form.Label>
-                    <Form.Control required type="text" placeholder="Title name" />
+                    <Form.Label>{data.title}</Form.Label>
+
                 </Form.Group>
                 {/* for subject*/}
                 <Form.Group className="mb-3" controlId="advertisementJournalSubject">
                     <Form.Label>Subject</Form.Label>
-                    <Form.Control required type="text" placeholder="Subject" />
+                    <Form.Label>{data.subject}</Form.Label>
                 </Form.Group>
                 {/* for publisher*/}
                 <Form.Group className="mb-3" controlId="advertisementJournalPublisher">
@@ -93,7 +137,8 @@ function ApprovalPDF() {
                     <Form.Group className="mb-3" controlId="formApproveJournal" >
 
 
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" type="submit" onClick={() => handleApprove('Approved')}>
+
                             Approve
                         </Button>
                     </Form.Group>
@@ -105,14 +150,16 @@ function ApprovalPDF() {
                     <Form.Group className="mb-3" controlId="formDeclineJournal" >
 
 
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" type="submit" onClick={() => handleApprove('Rejected')}>
+
                             Reject
                         </Button>
                     </Form.Group>
                 </Col>
                 <Col sm={3} />
             </Row>
-        </Form >
+        </Card >
+
 
     );
 }
