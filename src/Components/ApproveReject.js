b24d824d4fbef4dd18182522527e6d3ae5e6bcd1
jyoshna-joshi <g.jyoshnajoshi@gmail.com>
2023-05-15 18:38:59 +1000

    refinemeents

--- src/Components/ApproveReject.js
+++ src/Components/ApproveReject.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+// import {PDFViewer} from '@logicamente.info/react-pdf-viewer';
 
 function ApproveReject() {
-    const { state } = useLocation();
-    const { id, date, title } = state;
-    console.log(id);
-    console.log(title);
-    console.log(date);
-
-
+	const { state } = useLocation();
+	const { id, date, title } = state;
+	console.log(id);
+	console.log(title);
+	console.log(date)
 
 }
 
